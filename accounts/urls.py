485fe2ba611ba3bcf5ee36e93b1bc386c7b0f122
Anarpego <anibalperezgonzalez20@gmail.com>
2023-05-23 17:19:08 -0600

    webcam in the browser after selecting login with face but it's not recognizing yet

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('', views.home),
-    path("logout", views.logout_view)
+    path("logout", views.logout_view),
+    path('face_login/', views.face_login, name='face_login'),
 ]
